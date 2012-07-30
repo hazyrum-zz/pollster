96c5656144e8dc5acb2133f6a5cfa67c68a5fa7b
Mary Zhu <maryzhu@umich.edu>
2012-07-30 14:59:42 -0700

    cleanup

--- pollster/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Pollster::Application.config.secret_token = '6a2b3c265837d0f77cff716daa6e0637ab6d2d30d42fc0acbc8233f0b70c1b8ebf581230f4817efda4e55b25c6b79e48c3f687c224d22f0b0179ea22015e6dce'
